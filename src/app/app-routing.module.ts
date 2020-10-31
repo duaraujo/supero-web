0ec4f0bc499f36ae752e63c4ea335d4e3001d2f7
Eduardo Araujo <eduardo.araujo@timeitn.com.br>
2020-10-31 12:35:08 -0400

    creating home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { TaskComponent } from './view/task/task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'task',
+    component: TaskComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,24 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './view/header/header.component';
 import { FooterComponent } from './view/footer/footer.component';
+import { TaskComponent } from './view/task/task.component';
+import { HomeComponent } from './view/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TaskComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +31,8 @@ import { FooterComponent } from './view/footer/footer.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatCardModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
