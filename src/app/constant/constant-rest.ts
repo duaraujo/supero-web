b4e8c9d9b502401347471215c7140eb41acfc9f9
Eduardo Araujo <eduardo.araujo@timeitn.com.br>
2020-10-31 10:35:14 -0400

    creating task service

---
+++ src/app/constant/constant-rest.ts
@@ -0,0 +1,5 @@
+export const APP_URL = 'http://localhost:8080/';
+
+export const CONSTANT_URL = {
+    BASE_URL: APP_URL + 'supero/'
+};
---
+++ src/app/service/task.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CONSTANT_URL } from '../constant/constant-rest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  protected BASE_URL = CONSTANT_URL.BASE_URL;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public getAll(): Observable<any> {
+    return this.http.get(this.BASE_URL + 'task');
+  }
+
+}
