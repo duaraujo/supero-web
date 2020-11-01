03f4d0707778cda819bb1a9b3ee8c0977fe01c9b
Eduardo Araujo <eduardo.araujo@timeitn.com.br>
2020-10-31 22:09:24 -0400

    deploy heroku

--- src/app/constant/constant-rest.ts
+++ src/app/constant/constant-rest.ts
@@ -1,4 +1,5 @@
-export const APP_URL = 'http://localhost:8080/';
+// export const APP_URL = 'http://localhost:8080/';
+export const APP_URL = 'https://supero-back.herokuapp.com/';
 
 export const CONSTANT_URL = {
     BASE_URL: APP_URL + 'supero/'
